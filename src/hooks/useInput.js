6f74e18af54486f9c7fa2c4a01a0b8522edcf11e
Azsa Nurwahyudi <azsa.nwhy@gmail.com>
2022-12-20 10:30:50 +0700

    branch master protection test

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -7,7 +7,7 @@ function useInput(defaultValue = '') {
     setValue(target.value);
   }
 
-  return [value, handleValueChange, setValue];
+  return [value, abcde, setValue];
 }
 
 export default useInput;
